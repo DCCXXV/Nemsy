0cacd81cf7d263e38eda13dc532d6f456a6b7361
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2025-10-02 00:25:11 +0200

    fix(db): studies in schema

--- backend/internal/db/migrations/000001_init.down.sql
+++ backend/internal/db/migrations/000001_init.down.sql
@@ -1,8 +1,10 @@
 DROP INDEX IF EXISTS idx_resources_subject_created;
 DROP INDEX IF EXISTS idx_resources_owner_id;
 DROP INDEX IF EXISTS idx_resources_subject_id;
+DROP INDEX IF EXISTS idx_subjects_code;
+DROP INDEX IF EXISTS idx_subjects_study_id;
 
 DROP TABLE IF EXISTS resources;
-
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS subjects;
+DROP TABLE IF EXISTS studies;
+DROP TABLE IF EXISTS users;
